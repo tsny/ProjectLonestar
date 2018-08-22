d8f07680468b5d7b5acbfea683dd56805e35b2ee
tsny <tsny700@gmail.com>
2018-08-22 01:58:55 -0500

    Reverted to older version before accidental removal of editor files

---
+++ Assets/Editor/ShipEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Ship))]
+public class ShipEditor : Editor
+{
+    PlayerController playerController;
+    Ship ship;
+
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ship = target as Ship;
+
+        if (playerController.controlledShip != ship)
+        {
+            ShowInspectorPossessionControls();
+        }
+
+        else
+        {
+            ShowInspectorUnpossessionControls();
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        ship = target as Ship;
+
+        if (playerController.controlledShip != ship)
+        {
+            ShowPossessionHandle();
+        }
+    }
+
+    private void ShowPossessionHandle()
+    {
+        if (Handles.Button(ship.transform.position + Vector3.up * 10, Quaternion.identity, 3, 3, Handles.SphereHandleCap))
+        {
+            playerController.Possess(ship);
+        }
+    }
+
+    private void ShowInspectorPossessionControls()
+    {
+        if (GUILayout.Button("Possess"))
+        {
+            playerController.Possess(ship);
+        }
+    }
+
+    private void ShowInspectorUnpossessionControls()
+    {
+        if (GUILayout.Button("Unpossess"))
+        {
+            playerController.UnPossess();
+        }
+    }
+}
---
+++ Assets/Editor/TractorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TractorHardpoint))]
+public class TractorEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        TractorHardpoint tractor = (TractorHardpoint)target;
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("LootAll"))
+        {
+            tractor.TractorAllLoot();
+        }
+    }
+}
