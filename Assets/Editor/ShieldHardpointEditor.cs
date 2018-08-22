6f5a5090d7cabc5f2cc4abb37e4a82141938645c
tsny <tsny700@gmail.com>
2018-08-22 00:44:02 -0500

    Removed non-Source files

---
+++ Assets/Editor/ShieldHardpointEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShieldHardpoint))]
+public class ShieldHardpointEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ShieldHardpoint shieldHardpoint = (ShieldHardpoint)target;
+
+        EditorGUI.BeginChangeCheck();
+
+        Shield newShield = (Shield) EditorGUILayout.ObjectField("Shield", shieldHardpoint.CurrentEquipment, typeof(Shield), true);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (newShield == null)
+            {
+                shieldHardpoint.Demount();
+            }
+
+            shieldHardpoint.CurrentEquipment = newShield;
+        }
+
+        if (shieldHardpoint.IsMounted)
+        {
+            if (GUILayout.Button("Demount"))
+            {
+                shieldHardpoint.Demount();
+            }
+        }
+    }
+}
