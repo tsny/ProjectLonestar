134f27da026d216adda42a72fcdfab97addf0913
tsny <tsny700@gmail.com>
2018-09-18 11:42:36 -0500

    Dust effect is world space and doesn't inherit velocity (looks better)

--- Assets/Scripts/Ship/ShipEffectManager.cs
+++ Assets/Scripts/Ship/ShipEffectManager.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class ShipEffectManager : ShipComponent
 {
-    public GameObject[] engineEffects;
-    public GameObject chargeCruiseEffect;
-    public GameObject fullCruiseEffect;
-    public GameObject dustEffect;
+    public ParticleSystem[] engineEffects;
+    public ParticleSystem chargeCruiseEffect;
+    public ParticleSystem fullCruiseEffect;
+    public ParticleSystem dustEffect;
 
     public float currentScale = 1;
 
     private Vector3 originalScale;
-
-    private void Awake()
-    {
-        enabled = false;
-    }
 
     public override void InitShipComponent(Ship sender, ShipStats stats)
     {
         base.InitShipComponent(sender, stats);
         sender.cruiseEngine.CruiseStateChanged += HandleCruiseChanged;
         sender.Possession += HandleOwnerPossession;
-
-        chargeCruiseEffect.SetActive(false);
-        fullCruiseEffect.SetActive(false);
     }
 
     private void HandleOwnerPossession(PlayerController pc, Ship sender, bool possessed)
     {
-        dustEffect.SetActive(possessed);
+        if (possessed)
+        {
+            dustEffect.Play();
+        }
+
+        else
+        {
+            dustEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
     }
 
     private void HandleCruiseChanged(CruiseEngine sender)
@@ -38,42 +38,26 @@ public class ShipEffectManager : ShipComponent
         switch (sender.State)
         {
             case CruiseEngine.CruiseState.Off:
-                chargeCruiseEffect.SetActive(false);
-                fullCruiseEffect.SetActive(false);
+                chargeCruiseEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                fullCruiseEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
                 break;
 
             case CruiseEngine.CruiseState.Charging:
-                chargeCruiseEffect.SetActive(true);
+                chargeCruiseEffect.Play();
                 break;
 
             case CruiseEngine.CruiseState.On:
-                chargeCruiseEffect.SetActive(false);
-                fullCruiseEffect.SetActive(true);
+                chargeCruiseEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                fullCruiseEffect.Play();
                 break;
 
             case CruiseEngine.CruiseState.Disrupted:
-                chargeCruiseEffect.SetActive(false);
-                fullCruiseEffect.SetActive(false);
+                chargeCruiseEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                fullCruiseEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
                 break;
 
             default:
                 break;
-        }
-    }
-
-    private void Start()
-    {
-        // TODO: make this keep track of all orignal scales for each engine effect
-        originalScale = engineEffects[0].transform.localScale;
-        chargeCruiseEffect.SetActive(false);
-        fullCruiseEffect.SetActive(false);
-    }
-
-    private void Update()
-    {
-        foreach (var effect in engineEffects)
-        {
-            effect.transform.localScale = originalScale * owningShip.engine.Speed;
         }
     }
 }
