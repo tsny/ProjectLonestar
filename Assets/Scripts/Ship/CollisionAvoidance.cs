fe87d93b27ad7860cb89ddb520f97d941e4ec8a6
tsny <tsny700@gmail.com>
2018-10-04 22:43:26 -0500

    Minor refactoring in CollisionAvoidance.cs

--- Assets/Scripts/Ship/CollisionAvoidance.cs
+++ Assets/Scripts/Ship/CollisionAvoidance.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class CollisionAvoidance : ShipComponent
+public class CollisionAvoidance : ShipComponent 
 {
     public float rayCastOffset = 5;
     public float detectionDistance = 20;
@@ -11,60 +11,56 @@ public class CollisionAvoidance : ShipComponent
 
     public Engine engine;
 
-    private void Update()
+    private void Update () 
     {
-        if (engine == null)
-            return;
+        if (engine == null) return;
 
-        var obstacleDetected = CheckForObstacle();
+        var detectedAnObstacle = CheckForObstacle ();
 
-        if (obstacleDetected)
-            obstacleDetectedTime += Time.deltaTime;
+        if (detectedAnObstacle) obstacleDetectedTime += Time.deltaTime;
 
-        else
-            obstacleDetectedTime = 0;
+        else obstacleDetectedTime = 0;
     }
 
-    public bool CheckForObstacle()
+    public bool CheckForObstacle () 
     {
         RaycastHit hit;
 
-        Vector3 left = transform.position - transform.right * rayCastOffset;
+        Vector3 left  = transform.position - transform.right * rayCastOffset;
         Vector3 right = transform.position + transform.right * rayCastOffset;
-        Vector3 up = transform.position + transform.up * rayCastOffset;
-        Vector3 down = transform.position - transform.up * rayCastOffset;
+        Vector3 up    = transform.position + transform.up * rayCastOffset;
+        Vector3 down  = transform.position - transform.up * rayCastOffset;
 
-        Debug.DrawRay(left, detectionDistance * transform.forward, Color.cyan);
-        Debug.DrawRay(right, detectionDistance * transform.forward, Color.cyan);
-        Debug.DrawRay(up, detectionDistance * transform.forward, Color.cyan);
-        Debug.DrawRay(down, detectionDistance * transform.forward, Color.cyan);
+        Debug.DrawRay (left, detectionDistance * transform.forward, Color.cyan);
+        Debug.DrawRay (right, detectionDistance * transform.forward, Color.cyan);
+        Debug.DrawRay (up, detectionDistance * transform.forward, Color.cyan);
+        Debug.DrawRay (down, detectionDistance * transform.forward, Color.cyan);
 
-
-        if (Physics.Raycast(left, transform.forward, out hit, detectionDistance))
+        if (Physics.Raycast (left, transform.forward, out hit, detectionDistance)) 
         {
             engine.Strafe = 1;
             return true;
-        }
-
-        else if (Physics.Raycast(right, transform.forward, out hit, detectionDistance))
+        } 
+        
+        else if (Physics.Raycast (right, transform.forward, out hit, detectionDistance)) 
         {
             engine.Strafe = -1;
             return true;
-        }
+        } 
 
-        else if (Physics.Raycast(up, transform.forward, out hit, detectionDistance))
+        else if (Physics.Raycast (up, transform.forward, out hit, detectionDistance)) 
         {
-            engine.Pitch(-engine.engineStats.turnSpeed);
+            engine.Pitch (-engine.engineStats.turnSpeed);
             return true;
-        }
+        } 
 
-        else if (Physics.Raycast(down, transform.forward, out hit, detectionDistance))
+        else if (Physics.Raycast (down, transform.forward, out hit, detectionDistance)) 
         {
-            engine.Pitch(engine.engineStats.turnSpeed);
+            engine.Pitch (engine.engineStats.turnSpeed);
             return true;
-        }
+        } 
 
-        else
+        else 
         {
             engine.Strafe = 0;
             return false;
