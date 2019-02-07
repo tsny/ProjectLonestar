93e86e06188ca1fa3b0a43c2891222f91a3308b2
tsny <tsny700@gmail.com>
2019-02-06 19:09:34 -0600

    Deleted old files, Cleanup

--- Assets/Scripts/AI/Actions/GetRandomTarget.cs
+++ Assets/Scripts/AI/Actions/GetRandomTarget.cs
@@ -5,19 +5,10 @@ using System.Collections.Generic;
 [CreateAssetMenu(menuName = "AI/Actions/FindRandomTarget")]
 public class GetRandomTarget : FLAction
 {
-    public bool findOnlyShips = true;
-    public bool includeAllies;
-    // Select only allies?
-
     public override void Init() {}
 
     public override void Act(StateController controller)
     {
-        var targets = controller.enemies;
-        if (targets.Count < 1) return;
-
-        var rnd = new System.Random();
-        int r = rnd.Next(targets.Count); 
-        controller.Target = targets[r].gameObject;
+        controller.TargetRandomEnemy();
     }
 }
