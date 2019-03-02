f12c993d0dd1a717752cc3581b483844c0dfc67c
tsny <tsny700@gmail.com>
2019-03-02 13:35:12 -0600

    First pass on SceneHelper

--- Assets/Scripts/Core/SceneHelper.cs
+++ Assets/Scripts/Core/SceneHelper.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneHelper : MonoBehaviour 
 {
     public FadeCamera fade;
     public PlayerController pc;
+    public bool spawnPlayer;
 
     void Awake()
     {
@@ -12,13 +15,36 @@ public class SceneHelper : MonoBehaviour
 
     void Start()
     {
-        var ship = Instantiate(GameSettings.Instance.defaultShip, pc.transform.position, Quaternion.identity);
-        pc.Possess(ship);
-        Invoke("Test", 2);
+        if (spawnPlayer)
+        {
+            var ship = Instantiate(GameSettings.Instance.defaultShip, pc.transform.position, Quaternion.identity);
+            pc.Possess(ship);
+        }
+
+        Invoke("StartFading", 2);
     }
 
-    void Test()
+    public void StartFading()
     {
         fade.updateElapsed = true;
+    }
+    
+    public void FadeToScene(string scnName)
+    {
+        if (!IsSceneValid(scnName)) return;
+        StartCoroutine(FadeRoutine("SCN_Debug"));
+    }
+
+    bool IsSceneValid(string scnName)
+    {
+        Scene scn = SceneManager.GetSceneByName(scnName);
+        return scn.buildIndex != 1;
+    }
+
+    private IEnumerator FadeRoutine(string scnName)
+    {
+        fade.fadeIn = false;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(scnName);
     }
 }
